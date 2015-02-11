76a6c20d679f3110bde91fcd96b3425e8e453ba5
Tucker Mayo & Dex Fitch <student@epicodus.com>
2015-02-11 15:33:38 -0800

    correct frontend bugs

--- js/scripts.js
+++ js/scripts.js
@@ -77,54 +77,53 @@ $(document).ready(function() {
     while (index <= 8) {
       if (m > 0) {
         $("#roman-result").append(Array(m + 1).join("M"));
-        numbers.splice(0, 1);
+        m = 0;
 
       } else if ((d === 1) && (c === 4)) {
         $("#roman-result").append("CM");
-        numbers.splice(1, 1);
-        numbers.splice(2, 1);
+        d = 0;
+        c = 0;
 
       } else if ((l === 1 ) && (x === 4)) {
         $("#roman-result").append("XC");
-        numbers.splice(3, 1);
-        numbers.splice(4, 1);
+        l = 0;
+        x = 0;
 
       } else if ((v === 1) && (i === 4)) {
         $("#roman-result").append("IX");
-        numbers.splice(5, 1);
-        numbers.splice(6, 1);
+        v = 0;
+        i = 0;
+
 
       } else if ((v === 0) && (i === 4)) {
         $("#roman-result").append("IV");
-        numbers.splice(6, 1);
+        i = 0;
 
       } else if (d > 0) {
         $("#roman-result").append(Array(d + 1).join("D"));
-        numbers.splice(1, 1);
+        d = 0;
 
       } else if (c > 0) {
         $("#roman-result").append(Array(c + 1).join("C"));
-        numbers.splice(2, 1);
+        c = 0;
 
       } else if (l > 0) {
         $("#roman-result").append(Array(l + 1).join("L"));
-        numbers.splice(3, 1);
+        l = 0;
 
       } else if (x > 0) {
         $("#roman-result").append(Array(x + 1).join("X"));
-        numbers.splice(4, 1);
+        x = 0;
 
       } else if (v > 0) {
         $("#roman-result").append(Array(v + 1).join("V"));
-        numbers.splice(5, 1);
+        v = 0;
 
       } else if (i > 0) {
         $("#roman-result").append(Array(i + 1).join("I"));
-        numbers.splice(6, 1);
-      } else {
-        numbers.splice(0, 7);
-      };
-
+        i = 0;
+      }
+      
       index += 1;
 
     };
